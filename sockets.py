32b61e0f93fe1eddb83edba1788eb3b69c757587
googolplex <gglplxxx@gmail.com>
2021-11-21 04:26:40 +0300

    Handle exceptions

--- sockets.py
+++ sockets.py
@@ -12,14 +12,20 @@ async def disconnect(client, uri):
     clients[uri].remove(client)
 
 async def broadcast(uri, message):
-    for client in clients[uri]:
-        await client.send(message)
+    try:
+        for client in clients[uri]:
+            await client.send(message)
+    except:
+        await disconnect(client, uri)
 
 async def handler(client, uri):
     await connect(client, uri)
-    async for message in client:
-        print(uri, message)
-        await broadcast(uri, message)
+    try:
+        async for message in client:
+            print(uri, message)
+            await broadcast(uri, message)
+    except:
+        pass
     await disconnect(client, uri)
 
 async def main(stop):
@@ -30,5 +36,5 @@ loop = asyncio.get_event_loop()
 
 stop = loop.create_future()
 loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-        
+
 loop.run_until_complete(main(stop))
