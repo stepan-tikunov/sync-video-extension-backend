13bcffeddf20613f88e09e3eba975405044949fa
googolplex <gglplxxx@gmail.com>
2021-11-21 05:03:27 +0300

    Check if messages are valid, send users room size as they connect

--- sockets.py
+++ sockets.py
@@ -1,6 +1,16 @@
-import asyncio, signal, websockets
+import asyncio, signal, websockets, json
 
 clients = {}
+
+async def update_room_size(uri):
+    room_size = {
+        "type": "update_room_size",
+        "roomSize": len(clients[uri])
+    }
+
+    message = json.dumps(room_size, ensure_ascii=False)
+
+    await broadcast(uri, message)
 
 async def connect(client, uri):
     if uri in clients:
@@ -8,8 +18,12 @@ async def connect(client, uri):
     else:
         clients[uri] = [client]
 
+    await update_room_size(uri)
+        
 async def disconnect(client, uri):
     clients[uri].remove(client)
+
+    await update_room_size(uri)
 
 async def broadcast(uri, message):
     try:
@@ -22,8 +36,37 @@ async def handler(client, uri):
     await connect(client, uri)
     try:
         async for message in client:
-            print(uri, message)
-            await broadcast(uri, message)
+            try:
+                data = json.loads(message)
+                bad_message = any("type" not in data,
+                                  "videoId" not in data,
+                                  "time": not in data,
+                                  data["type"] not in ("pause", "play"))
+                try:
+                    int(data["videoId"])
+                    videoIdIsNotInt = False
+                except:
+                    videoIdIsNotInt = True
+
+                try:
+                    float(data["time"])
+                    timeIsNotFloat = False
+                except:
+                    timeIsNotFloat = False
+
+                bad_message = any(bad_message, videoIdIsNotInt, timeIsNotFloat)
+                if bad_message:
+                    pass
+                else:
+                    data = {
+                        "type": data["type"],
+                        "videoId": data["videoId"],
+                        "time": data["time"]
+                    }
+                    message = json.dumps(data, ensure_ascii=False)
+                    await broadcast(uri, message)
+            except:
+                pass
     except:
         pass
     await disconnect(client, uri)
